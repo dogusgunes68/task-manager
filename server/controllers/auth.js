e815e910d9c6062da942d059eff2eacffdfb16f6
dogusgunes68 <dogusgunes68@gmail.com>
2023-03-30 17:43:45 +0300

    adding task completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -31,6 +31,18 @@ exports.signup = async (req, res, next) => {
     console.log("user: ", user);
 
     const token = signJwt(user.id);
+
+    const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    };
+
+    res.cookie("jwt", token, cookieOptions);
+
+    user.password = undefined;
+
     res.status(201).json({
       status: "success",
       token,
